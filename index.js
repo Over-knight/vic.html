d0c6711bf9300ae8d3b635ba019bf6c2facac443
OVER_NIGHT <lilvickydsecond@gmail.com>
2022-10-05 10:46:32 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("nice job"); 
+console.log("nice job man");
+console.log("nice job guy");
+// first java script code 
+let name="mosh";
+console.log(name);
+let intrestRate= 0.3;
+intrestRate= 0.3;
+console.log(intrestRate);
